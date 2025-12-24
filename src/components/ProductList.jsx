2b106d378b7fb4607434d32356df0a65f32acfbb
avanyushkin <a.vanyushkin1@gmail.com>
2025-12-24 13:07:12 +0300

    feat: integrate dummyjson

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useDummyJSON } from '../utils/api.js';
 
 function ProductList() {
+  const {data, loading, error} = useDummyJSON('/products', { limit: 30 });
+
+  if (loading) {
+    return (
+      <>
+        <h2>Loading...</h2>
+      </>
+    );
+  }
+
+  if (!data) {
+    return (
+      <>
+        <h2>No products</h2>
+      </>
+    );
+  }
+
   return (
     <>
+      {data.products.map(product => (
+        <div key={product.id}>
+          <h3>{product.title}</h3>
+          <p>{product.price}</p>
+        </div>
+      ))}
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header.jsx';
-import ProductCard from '../components/ProductCard.jsx';
 import ProductList from '../components/ProductList.jsx';
 
 function HomePage() {
   return (
     <>
       <Header />
-      <ProductCard />
       <ProductList />
     </>
   );
